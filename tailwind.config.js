aa882382d258d2e5a5a7bba73ddb30aabd611069
Sahil Sharma <sahilsharma2000dtc@gmail.com>
2024-02-09 21:58:30 +0530

    modified tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}"], //iska mtlb hai src folder ke ander jitne bhi folder/files honge html/js in sbko trace karega baaki nhi karega.
   theme: {
     extend: {},
   },
